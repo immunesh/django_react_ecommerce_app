f63caa838c264e33db3bcc2e4082926512a21f67
iammunesh <munesh6181@gmail.com>
2024-11-18 21:01:56 +0530

    React Bootstrap setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
+import { Container } from 'react-bootstrap'
 
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   return (
     <div>
-      My App By Munesh
+      <Header />
+      <main className='py-3'>
+        <Container>
+        <h1>Welcome To Myshop</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
